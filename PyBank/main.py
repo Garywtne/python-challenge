60bce99065286cb4ee315dbd2e36624475406b94
Garywtne <gary.whitney@stiga.com>
2022-03-14 10:00:18 +0000

    further updates to code and commentary

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,44 @@
+# import dependacies
 
+# os to ensure that the correct operating system is recognised
 import os
+
+# csv to enable the reading of csv files
 import csv
 
 # Path to collect data from the Resources folder
 
 budget_csv = os.path.join('Resources', 'budget_data.csv')
+
 # print(budget_csv)
 
 # declare the variables to track and give them initial values
 
+# total_months initially set to zero then added to as looping through the data to provide the 'Total Months'
 month_total = 0
-month_change = []
-PL_change = []
-greatest_increase = ["", 0]
-greatest_decrease = ["", 9999999999999999999]
+
+# the total value starts at zero and will be added to when looping through the data to provide the 'Total'
 total = 0
+
+# we calulate the 'Average Change' by dividing the Total sum(PL_change) by the number of changes len(PL_change)
+
+# prev_PL to facilitate calculation of the change in profit/loss each month
+prev_PL = 0
+
+# This list starts empty and then as we loop through the data it is appened with the month_change
+month_change = []
+
+# This list also starts empty and then as we loop through the data it is appened PL_change associated with the month_change
+PL_change = []
+
+# This array has two elements, the first captures the month_change (currenty empty) and the secound the PL_change (set at zero) which enables us to calculate which month_change has the greatest increase 
+greatest_increase = ["", 0]
+
+# This array also has two elements, the first captures the month_change (currenty empty) and the secound the PL_change (set at a number greater than the maximum PL_change) 
+# this enables us to calculate which month_change has the greatest decrease
+
+greatest_decrease = ["", 2000000]
+
 
 
 # read the csv file and create a list of dictionarys
@@ -30,7 +54,11 @@ with open(budget_csv) as PL_data:
 
     # Remove the first row from the loop 
     first_row = next(reader)
+
+    # add 1 to month_total
     month_total += 1
+
+    # cast as intergers
     total += int(first_row[1])
     prev_PL = int(first_row[1])
 
@@ -62,7 +90,9 @@ with open(budget_csv) as PL_data:
 #  calculate the average change in profit/loss by dividing the total change by the number of changes
 average_change = sum(PL_change) / len(PL_change)
 
-#  print the output summary with the correct formatting 
+# print(max(PL_change))
+
+# print the output summary with the correct formatting 
 
 # print("Financial Analysis")
 # print("-------------------------")
